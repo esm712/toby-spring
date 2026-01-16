a40b33fa716b4b9666c058343c66d7515bcf8a4a
LeeSeungMin <0283esm@naver.com>
2026-01-16 21:09:56 +0900

    #1.1 UserDao UserDaoTest

---
+++ src/main/java/com/hokkom/test/UserDaoTest.java
@@ -0,0 +1,28 @@
+package com.hokkom.test;
+
+import com.hokkom.dao.UserDao;
+import com.hokkom.domain.User;
+
+import java.sql.SQLException;
+
+public class UserDaoTest {
+
+    public static void main(String[] args) throws SQLException, ClassNotFoundException {
+        UserDao dao = new UserDao();
+
+        User user = new User();
+        user.setId("lsm");
+        user.setName("이승민");
+        user.setPassword("123456");
+
+        dao.add(user);
+
+        System.out.println(user.getId() + " 등록 성공");
+
+        User user2 = dao.get(user.getId());
+        System.out.println(user2.getName());
+        System.out.println(user2.getPassword());
+
+        System.out.println(user2.getId() + " 조회 성공");
+    }
+}
