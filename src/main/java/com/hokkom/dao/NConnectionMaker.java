186ff6e838303b5d30a89c57c435afa45a04753b
LeeSeungMin <0283esm@naver.com>
2026-01-19 20:05:41 +0900

    #1.3.2 인터페이스의 도입

--- src/main/java/com/hokkom/dao/NConnectionMaker.java
+++ src/main/java/com/hokkom/dao/NConnectionMaker.java
@@ -4,12 +4,11 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
-public class SimpleConnectionMaker {
+public class NConnectionMaker implements ConnectionMaker {
 
-    public Connection makeNewConnection() throws ClassNotFoundException, SQLException {
+    public Connection makeConnection() throws ClassNotFoundException, SQLException {
         Class.forName("com.mysql.cj.jdbc.Driver");
         Connection c = DriverManager.getConnection("jdbc:mysql://localhost/springbook", "spring", "book");
         return c;
     }
-
 }
