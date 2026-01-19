b4058196213b1c55e9eab88d27c960f92348c127
LeeSeungMin <0283esm@naver.com>
2026-01-19 21:50:26 +0900

    #1.8.2 XML을 이용하는 애플리케이션 컨텍스트

--- src/main/java/com/hokkom/test/UserDaoTest.java
+++ src/main/java/com/hokkom/test/UserDaoTest.java
@@ -5,13 +5,17 @@ import com.hokkom.dao.UserDao;
 import com.hokkom.domain.User;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.support.GenericXmlApplicationContext;
 
 import java.sql.SQLException;
 
 public class UserDaoTest {
 
     public static void main(String[] args) throws SQLException, ClassNotFoundException {
-        ApplicationContext context = new AnnotationConfigApplicationContext(DaoFactory.class);
+        // ApplicationContext context = new AnnotationConfigApplicationContext(DaoFactory.class);
+        // XML 방식
+        ApplicationContext context = new GenericXmlApplicationContext("applicationContext.xml");
+
         UserDao dao = context.getBean("userDao", UserDao.class);
 
         User user = new User();
