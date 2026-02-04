7998883dc38e5698ba09864c0f2d8df5950394b5
LeeSeungMin <0283esm@naver.com>
2026-02-05 00:28:48 +0900

    #2.2.2 테스트의 효율적인 수행결과 결과 관리

---
+++ src/test/java/com/hokkom/dao/UserDaoTest.java
@@ -0,0 +1,36 @@
+package com.hokkom.dao;
+
+import com.hokkom.domain.User;
+import org.junit.jupiter.api.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import java.sql.SQLException;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+
+class UserDaoTest {
+
+    @Test
+    public void addAndGet() throws SQLException {
+        // ApplicationContext context = new AnnotationConfigApplicationContext(DaoFactory.class);
+        // XML 방식
+        ApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
+
+        UserDao dao = context.getBean("userDao", UserDao.class);
+
+        User user = new User();
+        user.setId("lsm");
+        user.setName("이승민");
+        user.setPassword("123456");
+
+        dao.add(user);
+
+        User user2 = dao.get(user.getId());
+
+        assertThat(user2.getName()).isEqualTo(user.getName());
+        assertThat(user2.getPassword()).isEqualTo(user.getPassword());
+
+    }
+}
