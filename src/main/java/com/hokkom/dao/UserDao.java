a1034da31d4bc8559cf74146110e0dbd9cb55771
LeeSeungMin <0283esm@naver.com>
2026-02-06 22:30:13 +0900

    #3.2 변하는 것과 변하지 않는 것

--- src/main/java/com/hokkom/dao/UserDao.java
+++ src/main/java/com/hokkom/dao/UserDao.java
@@ -80,26 +80,8 @@ public class UserDao {
     }
 
     public void deleteAll() throws SQLException {
-        Connection c = null;
-        PreparedStatement ps = null;
-        try {
-            c = dataSource.getConnection();
-            ps = c.prepareStatement("delete from users");
-            ps.executeUpdate();
-        } catch (SQLException e) {
-            throw e;
-        } finally {
-            if (ps != null) {
-                try {
-                    ps.close();
-                } catch (SQLException e) {}
-            }
-            if (c != null) {
-                try {
-                    c.close();
-                } catch (SQLException e) {}
-            }
-        }
+        StatementStrategy st = new DeleteAllStatement();
+        jdbcContextWithStatement(st);
     }
 
     public int getCount() throws SQLException {
@@ -131,6 +113,31 @@ public class UserDao {
                 try {
                     c.close();
                 } catch (SQLException e){}
+            }
+        }
+    }
+
+
+    public void jdbcContextWithStatement(StatementStrategy stmt) throws SQLException {
+        Connection c = null;
+        PreparedStatement ps = null;
+
+        try {
+            c = dataSource.getConnection();
+            ps = stmt.makePreparedStatement(c);
+            ps.executeUpdate();
+        } catch (SQLException e) {
+            throw e;
+        } finally {
+            if (ps != null) {
+                try {
+                    ps.close();
+                } catch (SQLException e) {}
+            }
+            if (c != null) {
+                try {
+                    c.close();
+                } catch (SQLException e) {}
             }
         }
     }
